c47af72bb424575aac618ac5b71125e56575954d
eduardsdn <eduard.sdn@gmail.com>
2023-06-26 15:20:58 +0200

    Added layout for Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './style.css';
 import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <div className='cards'>
+          <Card />
+        </div>
     </div>
   );
 }
